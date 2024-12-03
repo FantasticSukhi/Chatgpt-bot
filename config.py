4f87b4f0f84ddff3351341f8dea6f007881672a1
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-12-03 14:19:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
 UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "OFFICIAL_MAMBA_NETWORK")
 SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "OFFICIAL_MAMBA_SUPPORT")
 START_IMG :str = os.environ.get(
-    "START_IMG", "https://graph.org/file/d4412c7b411ca8da9e177.jpg"
+    "START_IMG", "https://envs.sh/CFW.jpg"
 )
 
 MONGO_DB_URI :str = os.environ.get(
