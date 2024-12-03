7ba64d8635d003aada2f600378111ae10304fa56
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-12-03 14:40:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-API_ID :int = int(os.environ.get("API_ID", None))
-API_HASH :str = os.environ.get("API_HASH", "")
-BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
+API_ID :int = int(os.environ.get("API_ID", 20353207))
+API_HASH :str = os.environ.get("API_HASH", "e5b2ac2f9c37bda345fa9fb5ade66961")
+BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "8084692844:AAEx72hiOrIEYs2neAitcpnGTR75iUWkTX0")
 UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "OFFICIAL_MAMBA_NETWORK")
 SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "OFFICIAL_MAMBA_SUPPORT")
 START_IMG :str = os.environ.get(
@@ -10,7 +10,7 @@ START_IMG :str = os.environ.get(
 
 MONGO_DB_URI :str = os.environ.get(
     "MONGO_DB_URI",
-    "",
+    "mongodb+srv://danishzain1637:papaspartan@cluster0.xdje3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
 OWNER_ID :int= os.environ.get("OWNER_ID", 6713994904)
 
